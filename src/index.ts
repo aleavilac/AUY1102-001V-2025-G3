2182c214003d30d89ab9497058412663b45c500f
aleavilac <ale.avilac@duocuc.cl>
2025-11-08 20:38:19 -0300

    correciones para slint

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const apiKey = '12345-abcde-67890-fghij';
 const githubToken = 'ghp_1234567890abcdefghijklmnopqrstuvwx';
 
 function fetchData() {
-   fetch(`https://api.example.com/data?api_key=${apiKey}`)
+  fetch(`https://api.example.com/data?api_key=${apiKey}`)
     .then((response) => response.json())
     .then((data) => console.log(data))
     .catch((error) => console.error('Error:', error));
